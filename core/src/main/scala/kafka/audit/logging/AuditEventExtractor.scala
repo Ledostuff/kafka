34c75618ce8e6a5e7de3178a2d390121e429961e
SBT-Martemyanov-IV <ledostuff@gmail.com>
2020-04-16 11:59:57 +0500

    Add audit-event extractors for more orthogonal api

---
+++ core/src/main/scala/kafka/audit/logging/AuditEventExtractor.scala
@@ -0,0 +1,146 @@
+package kafka.audit.logging
+
+import kafka.audit.logging.AuditEventExtractor.AuditEventExtractor
+import org.apache.kafka.common.acl.AclBindingFilter
+import org.apache.kafka.common.protocol.MessageUtil
+import org.apache.kafka.common.requests.{AlterConfigsRequest, CreateAclsRequest, CreateTopicsRequest, DeleteAclsRequest, DeleteTopicsRequest, DescribeAclsRequest, DescribeConfigsRequest}
+import org.apache.kafka.common.resource.{ResourcePattern, ResourcePatternFilter}
+
+import scala.collection.JavaConverters._
+
+object AuditEventExtractor {
+
+  sealed trait AuditEventExtractor[IN] {
+    def extractStringRepr(in: IN): String
+  }
+
+  implicit object CreateAclsRequestAuditEventExtractor extends AuditEventExtractor[CreateAclsRequest] {
+    def extractStringRepr(request: CreateAclsRequest): String = {
+      request.aclCreations().asScala.map { creation =>
+        val entry = creation.acl().entry()
+        val pattern = creation.acl().pattern()
+        val patternString = AclPatternExtractor.extractStringRepr(pattern)
+        s"""(acl=(entry=${entry.toString}, pattern=${patternString}))"""
+      }.mkString("[", ", ", "]")
+    }
+  }
+
+  implicit object DeleteAclsRequestAuditEventExtractor extends AuditEventExtractor[DeleteAclsRequest] {
+    def extractStringRepr(request: DeleteAclsRequest): String = {
+      request.filters().asScala.map { filter =>
+        AclBindingFilterExtractor.extractStringRepr(filter)
+      }.mkString("[", ", ", "]")
+    }
+  }
+
+  implicit object DescribeAclsRequestAuditEventExtractor extends AuditEventExtractor[DescribeAclsRequest] {
+    def extractStringRepr(request: DescribeAclsRequest): String = {
+      AclBindingFilterExtractor.extractStringRepr(request.filter())
+    }
+  }
+
+  implicit object AlterConfigsRequestAuditEventExtractor extends AuditEventExtractor[AlterConfigsRequest] {
+    def extractStringRepr(request: AlterConfigsRequest): String = {
+      request.configs().asScala.mapValues { conf =>
+        s"[${conf.entries().asScala.map(e => s"${e.name()} -> ${e.value()}").mkString("[", ", ", "]")}]"
+      }.mkString("[", ", ", "]")
+    }
+  }
+
+  implicit object DescribeConfigsRequestAuditEventExtractor extends AuditEventExtractor[DescribeConfigsRequest] {
+    def extractStringRepr(request: DescribeConfigsRequest): String = {
+      request.resources().asScala.map { resource =>
+        s"type='${resource.`type`()}', name='${resource.name()}')"
+      }.mkString("[", ", ", "]")
+    }
+  }
+
+  implicit object CreateTopicsRequestAuditEventExtractor extends AuditEventExtractor[CreateTopicsRequest] {
+    def extractStringRepr(request: CreateTopicsRequest): String = {
+      request.data().topics().valuesList().asScala.map { creatableTopic =>
+        "name=" + (if (creatableTopic.name() == null) "null"
+        else "'" + creatableTopic.name() + "'") +
+          ", numPartitions=" + creatableTopic.numPartitions() +
+          ", replicationFactor=" + creatableTopic.replicationFactor() +
+          ", assignments=" + MessageUtil.deepToString(creatableTopic.assignments().iterator) +
+          ", configs=" + MessageUtil.deepToString(creatableTopic.configs().iterator) +
+          ")"
+      }.mkString("[", ", ", "]")
+    }
+  }
+
+  implicit object DeleteTopicsRequestAuditEventExtractor extends AuditEventExtractor[DeleteTopicsRequest] {
+    def extractStringRepr(request: DeleteTopicsRequest): String = {
+      request.data().topicNames().asScala.mkString("[", ", ", "]")
+    }
+  }
+
+  private object AclBindingFilterExtractor extends AuditEventExtractor[AclBindingFilter] {
+    def extractStringRepr(filter: AclBindingFilter): String = {
+      val entry = filter.entryFilter()
+      val pattern = filter.patternFilter()
+      val patternString = AclPatternFilterExtractor.extractStringRepr(pattern)
+      s"""(entry=${entry.toString}, pattern=${patternString})"""
+    }
+  }
+
+  private object AclPatternExtractor extends AuditEventExtractor[ResourcePattern] {
+    def extractStringRepr(pattern: ResourcePattern): String = {
+      s"""(${pattern.resourceType}, name=${(if (pattern.name == null) "<any>" else pattern.name)}, patternType=${pattern.patternType})"""
+    }
+  }
+
+  private object AclPatternFilterExtractor extends AuditEventExtractor[ResourcePatternFilter] {
+    def extractStringRepr(pattern: ResourcePatternFilter): String = {
+      s"""(${pattern.resourceType}, name=${(if (pattern.name == null) "<any>" else pattern.name)}, patternType=${pattern.patternType})"""
+    }
+  }
+}
+
+object AuditEventExtractorSyntax {
+
+
+
+  implicit class CreateAclsRequestAuditEventExtractorSyntax(val request: CreateAclsRequest) {
+    def extractAuditEventPayload(implicit extractor: AuditEventExtractor[CreateAclsRequest]): String = {
+      extractor.extractStringRepr(request)
+    }
+  }
+
+  implicit class DeleteAclsRequestAuditEventExtractorSyntax(val request: DeleteAclsRequest) {
+    def extractAuditEventPayload(implicit extractor: AuditEventExtractor[DeleteAclsRequest]): String = {
+      extractor.extractStringRepr(request)
+    }
+  }
+
+  implicit class DescribeAclsRequestAuditEventExtractorSyntax(val request: DescribeAclsRequest) {
+    def extractAuditEventPayload(implicit extractor: AuditEventExtractor[DescribeAclsRequest]): String = {
+      extractor.extractStringRepr(request)
+    }
+  }
+
+  implicit class AlterConfigsRequestAuditEventExtractorSyntax(val request: AlterConfigsRequest) {
+    def extractAuditEventPayload(implicit extractor: AuditEventExtractor[AlterConfigsRequest]): String = {
+      extractor.extractStringRepr(request)
+    }
+  }
+
+  implicit class DescribeConfigsRequestAuditEventExtractorSyntax(val request: DescribeConfigsRequest) {
+    def extractAuditEventPayload(implicit extractor: AuditEventExtractor[DescribeConfigsRequest]): String = {
+      extractor.extractStringRepr(request)
+    }
+  }
+
+  implicit class CreateTopicsRequestAuditEventExtractorSyntax(val request: CreateTopicsRequest) {
+    def extractAuditEventPayload(implicit extractor: AuditEventExtractor[CreateTopicsRequest]): String = {
+      extractor.extractStringRepr(request)
+    }
+  }
+
+  implicit class DeleteTopicsRequestAuditEventExtractorSyntax(val request: DeleteTopicsRequest) {
+    def extractAuditEventPayload(implicit extractor: AuditEventExtractor[DeleteTopicsRequest]): String = {
+      extractor.extractStringRepr(request)
+    }
+  }
+
+}
--- core/src/main/scala/kafka/audit/logging/RequestToLogSerializer.scala
+++ core/src/main/scala/kafka/audit/logging/RequestToLogSerializer.scala
@@ -1,34 +1,33 @@
 package kafka.audit.logging
 
+import kafka.audit.logging.AuditEventExtractor._
+import kafka.audit.logging.AuditEventExtractorSyntax._
 import kafka.utils.Logging
-import org.apache.kafka.common.requests.{AbstractRequest, AlterConfigsRequest, CreateAclsRequest, CreateTopicsRequest, DeleteAclsRequest, DeleteTopicsRequest, DescribeAclsRequest, DescribeConfigsRequest, RequestContext}
-import scala.collection.JavaConverters._
+import org.apache.kafka.common.requests._
 
 object RequestToAuditLog extends Logging {
   def logRequest[T <: AbstractRequest](request: T, context: RequestContext): Unit = {
     request match {
       case createAclsRequest: CreateAclsRequest =>
-        val createAclEntity = createAclsRequest.aclCreations().asScala.mkString("[", ", ", "]")
+        val createAclEntity = createAclsRequest.extractAuditEventPayload
         debug(CommonAuditRequestToLogSerializer.serializeRequest(context, AuditEventName.CreateAcl, createAclEntity))
       case deleteAclsRequest: DeleteAclsRequest =>
-        val deleteAclEntity = deleteAclsRequest.filters().asScala.mkString("[", ", ", "]")
+        val deleteAclEntity = deleteAclsRequest.extractAuditEventPayload
         debug(CommonAuditRequestToLogSerializer.serializeRequest(context, AuditEventName.DeleteAcl, deleteAclEntity))
       case describeAclsRequest: DescribeAclsRequest =>
-        val describeAclsEntity = describeAclsRequest.filter().toString
+        val describeAclsEntity = describeAclsRequest.extractAuditEventPayload
         debug(CommonAuditRequestToLogSerializer.serializeRequest(context, AuditEventName.DescribeAcl, describeAclsEntity))
       case alterConfigsRequest: AlterConfigsRequest =>
-        val alterConfigsEntity = alterConfigsRequest.configs().asScala.mapValues { conf =>
-          s"[${conf.entries().asScala.map(e => s"${e.name()} -> ${e.value()}").mkString("[", ", ", "]")}]"
-        }.mkString("[", ", ", "]")
+        val alterConfigsEntity = alterConfigsRequest.extractAuditEventPayload
         debug(CommonAuditRequestToLogSerializer.serializeRequest(context, AuditEventName.AlterConfigs, alterConfigsEntity))
       case describeConfigsRequest: DescribeConfigsRequest =>
-        val describeConfigsEntity = describeConfigsRequest.resources().asScala.mkString("[", ", ", "]")
+        val describeConfigsEntity = describeConfigsRequest.extractAuditEventPayload
         debug(CommonAuditRequestToLogSerializer.serializeRequest(context, AuditEventName.DescribeConfigs, describeConfigsEntity))
       case createTopicsRequest: CreateTopicsRequest =>
-        val createTopicsEntity = createTopicsRequest.data().topics().valuesList().asScala.mkString("[", ", ", "]")
+        val createTopicsEntity = createTopicsRequest.extractAuditEventPayload
         debug(CommonAuditRequestToLogSerializer.serializeRequest(context, AuditEventName.CreateTopic, createTopicsEntity))
       case deleteTopicsRequest: DeleteTopicsRequest =>
-        val deleteTopicsEntity = deleteTopicsRequest.data().topicNames().asScala.mkString("[", ", ", "]")
+        val deleteTopicsEntity = deleteTopicsRequest.extractAuditEventPayload
         debug(CommonAuditRequestToLogSerializer.serializeRequest(context, AuditEventName.DeleteTopic, deleteTopicsEntity))
       case other => warn(s"Unexpected request type to log to audit logger - '${other.getClass.getCanonicalName}'.")
     }
